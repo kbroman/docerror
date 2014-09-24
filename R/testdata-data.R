a9ad0c7f10e9b5d1f629dcc43ff2c08e4c142514
Karl Broman <kbroman@gmail.com>
2014-09-24 10:01:34 -0500

    Initial commit

---
+++ R/testdata-data.R
@@ -0,0 +1,15 @@
+#' test dataset
+#'
+#' a dataset for testing use of Roxygen2 to document datasets
+#'
+#' @docType data
+#'
+#' @usage data(testdata)
+#'
+#' @format Just the number \code{0}.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(testdata)
+"testdata"
